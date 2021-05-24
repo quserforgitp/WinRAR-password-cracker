69942fb580740fef4b0d6c6384525d2fb0852208
AutHor <adelaumnonegligas@gmail.com>
2021-05-24 07:41:09 -0500

    CREACION DE 2 ARCHIVOS PRINCIPALES EL PRIMERO QUE PRIMERO -> winrarpasscrack.bat -> es el archivo principal SEGUNDO -> InstallWinrar.bat -> es el que instala winrar en caso de que 		no se encuentre en el equipo

---
+++ winrarpasscrack.bat
@@ -0,0 +1,9 @@
+REM settting all the batch configuration
+@ECHO OFF
+TITLE WINRAR PASSWORD CRACKER
+COLOR e
+
+
+
+REM este pause es para controlar el flujo del programa mientras se termina de codificar por completo 
+pause
