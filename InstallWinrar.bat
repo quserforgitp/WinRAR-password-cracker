12b969de3b42c648f63723ff54b91b9841adf968
AutHor <adelaumnonegligas@gmail.com>
2021-05-24 08:27:58 -0500

    Se ha añadido el menú de seleccion con las opciones ingles y español version 6.01 y arquitecturas x64

--- InstallWinrar.bat
+++ InstallWinrar.bat
@@ -11,5 +11,13 @@ SET embellishFinal=_-----
 REM promting to user select the version of winrar that he wnats to install..
 COLOR e 
 ECHO %spc%%embellishInit%WHICH VERSION DO YOU PREFER?%embellishFinal%
+ECHO.
+ECHO.
+ECHO.
+ECHO ^|LANGUAGE ^|%spc%^|VERSION^|%spc%^|ARCHITECTURE^|
+ECHO ^|1)SPANISH^|%spc%^|6.01   ^|%spc%^|   x64      ^|
+ECHO ^|2)ENGLISH^|%spc%^|6.01   ^|%spc%^|   x64      ^|
 
+
+REM this pause has been setted in order to control the program flow until finalized
 PAUSE
