5aacd315ebe34b0e1f96080338fdd5104770b13a
AutHor <adelaumnonegligas@gmail.com>
2021-06-15 11:14:59 -0500

    Se añadio la estructura basica al cracker.bat y se añadio el archivo table.md en donde se va a definir la tabla para tomar los caracteres y combinarlos para descifrar la contraseña

---
+++ cracker.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+REM ----------------------------------------------------------------------------
+REM |                                                                          |
+REM |                                                                          |
+REM |                 THIS PROGRAM IS A PART OF WINRARPASSCRACK SUITE          |
+REM |                 AUTHOR: AutHOr                                           |
+REM |                 GITHUB: https://github.com/quserforgitp                  |
+REM |                 THIS PROGRAM WAS CREATED FOR MAKE THE BFA TO A WINRAR    |
+REM |                                                                          |
+REM |                                                                          |
+REM |                                                                          |
+REM |                                                                          |
+REM |                                                                          |                                                            
+REM ----------------------------------------------------------------------------
+
+REM CONSTANTS declaring CONSTANTS for space control, AND for embellish text
+SET spc=                
+SET embellishInit=-----_
+SET embellishFinal=_-----
+
+TITLE %spc%%embellisini%BFA ATTACKER (THE REAL CRACKER)%embellishFinal%
+CLS
+COLOR E
+
+
+:EOF
+EXIT /B 0
